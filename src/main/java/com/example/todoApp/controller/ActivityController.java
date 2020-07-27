8ce189a646f432e09d7e8c9050c718b55df9685a
Mahira <soabmahira@gmail.com>
2020-07-27 22:29:43 +0400

    Removing comments

--- src/main/java/com/example/todoApp/controller/ActivityController.java
+++ src/main/java/com/example/todoApp/controller/ActivityController.java
@@ -1,11 +1,8 @@
 package com.example.todoApp.controller;
 
-import com.example.todoApp.model.Activity;
 import com.example.todoApp.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/example/todoApp/controller/BuildingController.java
+++ src/main/java/com/example/todoApp/controller/BuildingController.java
@@ -3,7 +3,6 @@ package com.example.todoApp.controller;
 import com.example.todoApp.model.Building;
 import com.example.todoApp.repository.BuildingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
